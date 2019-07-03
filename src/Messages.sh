d96b91c417f48d6ed60810e27852d015c35cdfad
Yuri Chornoivan <yurchor@ukr.net>
2019-07-03 08:23:28 +0300

    Fix typo and messages extraction

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACT_TR_STRINGS `find . -name \*.qml` -o $podir/calindori_qt.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/calindori_qt.pot
 rm -f rc.cpp
 
