8b10391eae45eeceeb254fa2b1b07307f29066e8
Dimitris Kardarakos <dimkard@posteo.net>
2021-03-03 16:11:49 +0200

    Fix translations configuration
    Calindori was configured as if it was using the Qt translation system and the .po files were compiled into .qm files, while Calindori code uses i18n.

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,5 +2,5 @@
 # SPDX-FileCopyrightText: 2019 Dimitris Kardarakos <dimkard@posteo.net>
 # SPDX-License-Identifier: BSD-2-Clause
 
-$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/calindori_qt.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/calindori.pot
 rm -f rc.cpp
