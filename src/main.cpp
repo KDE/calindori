54ea2c62b84b228838d2227e09cf8b2941a03349
Bhushan Shah <bhush94@gmail.com>
2021-05-10 12:34:03 +0530

    Adapt version to PlaMo Gear 21.05

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@
 #include "datahandler.h"
 #include "calendarcontroller.h"
 #include "attendeesmodel.h"
+#include "version.h"
 
 void handleArgument(DataHandler *dataHandler, const QStringList &args)
 {
@@ -51,7 +52,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("calindori");
 
-    KAboutData aboutData(QStringLiteral("calindori"), i18n("Calindori"), QStringLiteral("1.4.90"), i18nc("@title", "Calendar application"), KAboutLicense::GPL_V3, i18nc("@info:credit", "(c) 2018-2021 The Calindori Team"));
+    KAboutData aboutData(QStringLiteral("calindori"), i18n("Calindori"), QStringLiteral(CALINDORI_VERSION_STRING), i18nc("@title", "Calendar application"), KAboutLicense::GPL_V3, i18nc("@info:credit", "(c) 2018-2021 The Calindori Team"));
 
     aboutData.setOrganizationDomain(QByteArray("kde.org"));
     aboutData.setProductName(QByteArray("calindori"));
